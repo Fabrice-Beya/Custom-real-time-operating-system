858e057fb32485bd4d3d0e3a83a797b629d035d8
Fabrice Beya <beyafabrice@gmail.com>
2022-08-14 13:43:16 +0200

    Created LED drivers for discovery board

---
+++ led-driver/Debug/Src/main.d
@@ -0,0 +1,18 @@
+Src/main.o: ../Src/main.c ../Src/led.h \
+ /Users/fabricebeya/Documents/Projects/rtos/rtos-custom/led-driver/chip-headers/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /Users/fabricebeya/Documents/Projects/rtos/rtos-custom/led-driver/chip-headers/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ /Users/fabricebeya/Documents/Projects/rtos/rtos-custom/led-driver/chip-headers/Include/core_cm4.h \
+ /Users/fabricebeya/Documents/Projects/rtos/rtos-custom/led-driver/chip-headers/Include/cmsis_version.h \
+ /Users/fabricebeya/Documents/Projects/rtos/rtos-custom/led-driver/chip-headers/Include/cmsis_compiler.h \
+ /Users/fabricebeya/Documents/Projects/rtos/rtos-custom/led-driver/chip-headers/Include/cmsis_gcc.h \
+ /Users/fabricebeya/Documents/Projects/rtos/rtos-custom/led-driver/chip-headers/Include/mpu_armv7.h \
+ /Users/fabricebeya/Documents/Projects/rtos/rtos-custom/led-driver/chip-headers/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+../Src/led.h:
+/Users/fabricebeya/Documents/Projects/rtos/rtos-custom/led-driver/chip-headers/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/Users/fabricebeya/Documents/Projects/rtos/rtos-custom/led-driver/chip-headers/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+/Users/fabricebeya/Documents/Projects/rtos/rtos-custom/led-driver/chip-headers/Include/core_cm4.h:
+/Users/fabricebeya/Documents/Projects/rtos/rtos-custom/led-driver/chip-headers/Include/cmsis_version.h:
+/Users/fabricebeya/Documents/Projects/rtos/rtos-custom/led-driver/chip-headers/Include/cmsis_compiler.h:
+/Users/fabricebeya/Documents/Projects/rtos/rtos-custom/led-driver/chip-headers/Include/cmsis_gcc.h:
+/Users/fabricebeya/Documents/Projects/rtos/rtos-custom/led-driver/chip-headers/Include/mpu_armv7.h:
+/Users/fabricebeya/Documents/Projects/rtos/rtos-custom/led-driver/chip-headers/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
---
+++ led-driver/Src/main.c
@@ -0,0 +1,49 @@
+#include "led.h"
+
+void delay(uint32_t val)
+{
+	for(int i = 0; i < val; i++)
+	{
+
+	}
+}
+
+void short_delay()
+{
+	delay(1000000);
+}
+
+void long_delay()
+{
+	delay(10000000);
+}
+
+int main(void)
+{
+	LED_Typedef* orange = led_init(LED_3);
+	LED_Typedef* green = led_init(LED_4);
+	LED_Typedef* red = led_init(LED_5);
+	LED_Typedef* blue = led_init(LED_6);
+
+	while(1)
+	{
+		led_on(orange);
+		short_delay();
+		led_on(green);
+		short_delay();
+		led_on(red);
+		short_delay();
+		led_on(blue);
+
+		long_delay();
+
+		led_off(orange);
+		short_delay();
+		led_off(green);
+		short_delay();
+		led_off(red);
+		short_delay();
+		led_off(blue);
+		short_delay();
+	}
+}
